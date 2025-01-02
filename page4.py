0201a9fdf3a1802195e71affba1ce10591a5191d
kadyrovasafie <kadyrovasafie67@gmail.com>
2025-01-02 17:06:39 +0000

    contact us

--- page4.py
+++ page4.py
@@ -1,5 +1,27 @@
 import streamlit as st
 
 def app():
-    st.title("Page 4")
+    st.title("Contact us")
     st.write("This is the content of Page 4.")
+# Запит 4 імен та відповідних Gmail-адрес
+names = []
+emails = []
+
+for i in range(4):
+    name = input(f"Сафіє {i + 1}: ")
+    email = input(f"kadyrovasafie67@gmail.com {name}: ")
+    
+    # Перевірка, чи це Gmail-адреса
+    if "@gmail.com" not in email:
+        print("Будь ласка, введіть коректну Gmail-адресу.")
+        email = input(f"Повторно введіть Gmail для {name}: ")
+    
+    names.append(name)
+    emails.append(email)
+
+# Виведення імен та Gmail-адрес
+print("\nВведені дані:")
+for i in range(4):
+    print(f"Ім'я: {names[i]}, Gmail: {emails[i]}")
+
+ 
